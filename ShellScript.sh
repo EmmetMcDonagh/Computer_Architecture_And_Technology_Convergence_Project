82e934cd1bbc5fe7135eab2844b3b587a45f08a7
EmmetMcDonagh <109392395+EmmetMcDonagh@users.noreply.github.com>
2022-07-16 17:22:14 +0100

    Add files via upload

---
+++ ShellScript.sh
@@ -0,0 +1,44 @@
+echo 
+echo Command: date
+date
+echo Used to change the password of a user account.
+echo
+echo Command: hostname
+hostname
+echo Used to view a computer’s hostname and domain name.
+echo
+echo Command: arch
+arch
+echo It is used to print machine hardware name.
+echo
+echo Command: uname -a
+uname -a
+echo Prints basic information about a computer’s software and hardware.
+echo
+echo Command: uptime
+uptime
+echo It gives you the time for which the system has been running.
+echo
+echo Command: whoami
+whoami
+echo Prints the username of the current user. In other words, it displays the name of the currently logged-in user.
+echo
+echo Command: who
+who
+echo Prints a list of who is logged in. It can also show the current run level, time of the last system boot, and more.
+echo
+echo Command: finger
+finger
+echo User information lookup command used in Linux to check the information of any currently logged in users from the terminal.
+echo
+echo Command: w
+w
+echo Displays summary information related to users that are currently logged in, what each user is currently doing, and what load all the activity is imposing on the computer itself. 
+echo
+echo Command: top
+top -b -n 1
+echo It displays the Linux tasks. 
+echo
+echo Command: history
+history
+echo It shows the user history of all commands which were typed in terminal.
